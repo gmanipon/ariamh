6eae9ad6350ec20e848a4de7e8e05b4dd06bb2a0
gmanipon <gmanipon@jpl.nasa.gov>
2017-10-13 01:41:23 +0000

    pull slcp2pm from public github

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,4 +4,4 @@
 git clone https://github.com/hysds/spyddder-man.git
 
 # clone slcp2pm to be moved to its final location by docker builder
-git clone https://${GIT_OAUTH_TOKEN}@github.jpl.nasa.gov/aria-hysds/slcp2pm.git
+git clone https://github.com/hysds/slcp2pm.git
